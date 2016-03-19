ed70090215cd7caa7add16a008eed9fecca59eb2
David Torres <david@jelp.io>
2016-03-18 23:25:29 -0700

    Obtener los usuarios usando mongoose :)

--- controllers/v1.js
+++ controllers/v1.js
@@ -1,14 +1,20 @@
 var express = require('express'),
     app = express(),
     router = express.Router()
-    User = require('../models/User');
+    User = require('../models/User'),
+    mongoose = require('mongoose');
+
+mongoose.connect('mongodb://192.168.33.25:27017/test');
 
 router.get('/',function(req,res){
   res.send('V1 Router');
 });
 
 router.get('/users',function(req,res){
-  res.sen(User);
+  User.find(function (err,users) {
+    if (err) return console.error(err);
+    res.send(users);
+  });
 });
 
 module.exports = router;
